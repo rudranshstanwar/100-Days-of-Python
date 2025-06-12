de6dccb3f87e892805ac9436c3c36325200d7f66
Rudransh Tanwar <rudranshstanwar@gmail.com>
2025-06-12 09:53:03 +0530

    Day 4 - A Game of Rock Paper Scissors

---
+++ Day 4/Banker Roulette/task.py
@@ -0,0 +1,14 @@
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+import random
+random_guy = random.randint(1,5)
+if random_guy == 1:
+    print(friends[0])
+elif random_guy == 2:
+    print(friends[1])
+elif random_guy == 3:
+    print(friends[2])
+elif random_guy == 4:
+    print(friends[3])
+else:
+    print(friends[4])
+
